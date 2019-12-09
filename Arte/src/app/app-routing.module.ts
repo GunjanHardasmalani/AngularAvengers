cab56dd88e000c023f8c1df024714fbcb0162a60
Mayur <bhatia.ma@husky.neu.edu>
2019-12-09 03:55:26 -0500

    add login route

--- Arte/src/app/app-routing.module.ts
+++ Arte/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
+import { LoginComponent } from './login/login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+
+  // redirect to root path
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
